718d1c4e27138dfa1ee6f6601b22d2198289b75c
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 21:21:53 +0530

    feat(exception): implement base CustomException class for application errors

--- src/main/java/lk/ijse/aad_project/exception/DuplicateRecordException.java
+++ src/main/java/lk/ijse/aad_project/exception/DuplicateRecordException.java
@@ -3,5 +3,5 @@ package lk.ijse.aad_project.exception;
 public class DuplicateRecordException extends RuntimeException {
     public DuplicateRecordException(String message) {
         super(message);
-    }
+    } 
 }
