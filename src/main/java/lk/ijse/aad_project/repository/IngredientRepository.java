2d2dde49a6f85234dfbcb23f48bc4463e7a54d13
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:03:38 +0530

    feat(repository): create IngredientRepository for inventory management

--- src/main/java/lk/ijse/aad_project/repository/IngredientRepository.java
+++ src/main/java/lk/ijse/aad_project/repository/IngredientRepository.java
@@ -1,7 +1,7 @@
 package lk.ijse.aad_project.repository;
 
 import lk.ijse.aad_project.entity.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository; 
 import org.springframework.stereotype.Repository; 
 
 @Repository
