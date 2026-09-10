0b79d227549135d0e3f375778cc311bfec6a3e76
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 20:19:14 +0530

    add table reservation and booking endpoints

--- src/main/java/lk/ijse/aad_project/controller/ReservationController.java
+++ src/main/java/lk/ijse/aad_project/controller/ReservationController.java
@@ -1,7 +1,7 @@
 package lk.ijse.aad_project.controller;
 
 import lk.ijse.aad_project.dto.ReservationDTO;
-import lk.ijse.aad_project.service.ReservationService;
+import lk.ijse.aad_project.service.ReservationService; 
 import lk.ijse.aad_project.contant.CommonResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
