4eee45ea913d23e12129aead8a9508e37d74a7e5
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:13:43 +0530

    feat(repository): implement OrderRepository with customer order search queries

--- src/main/java/lk/ijse/aad_project/repository/OrderRepository.java
+++ src/main/java/lk/ijse/aad_project/repository/OrderRepository.java
@@ -2,8 +2,8 @@ package lk.ijse.aad_project.repository;
 
 import lk.ijse.aad_project.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository; 
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-}
+} 
