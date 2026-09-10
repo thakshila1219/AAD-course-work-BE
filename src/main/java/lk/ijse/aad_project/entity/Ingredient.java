3306aa3b4af876e62a178ca2ec7563da30e11499
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 20:33:38 +0530

    feat(entity): create Ingredient entity for stock inventory tracking

--- src/main/java/lk/ijse/aad_project/entity/Ingredient.java
+++ src/main/java/lk/ijse/aad_project/entity/Ingredient.java
@@ -2,7 +2,7 @@ package lk.ijse.aad_project.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Data; 
 import lombok.NoArgsConstructor;
 import java.util.List;
 
