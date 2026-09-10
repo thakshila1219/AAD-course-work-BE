f7bd35d3349f96ee3c867318d9c8d0dd104ab72b
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:10:53 +0530

    feat(repository): add MenuItemRepository for menu queries

--- src/main/java/lk/ijse/aad_project/repository/MenuItemRepository.java
+++ src/main/java/lk/ijse/aad_project/repository/MenuItemRepository.java
@@ -4,6 +4,6 @@ import lk.ijse.aad_project.entity.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository 
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 }
