547ca49150c1ea4ead9c951d0ca9c1c1b99486ae
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 21:34:13 +0530

    feat(repository): implement DiscountCouponRepository with custom search methods

--- src/main/java/lk/ijse/aad_project/repository/IngredientRepository.java
+++ src/main/java/lk/ijse/aad_project/repository/IngredientRepository.java
@@ -2,7 +2,7 @@ package lk.ijse.aad_project.repository;
 
 import lk.ijse.aad_project.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository; 
 
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
