7e2ea8b13ee0c30622b2730bcf2243efb2bd6514
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:50:20 +0530

    feat(service): update UserService interface with user profile operations

--- src/main/java/lk/ijse/aad_project/service/UserService.java
+++ src/main/java/lk/ijse/aad_project/service/UserService.java
@@ -2,7 +2,7 @@ package lk.ijse.aad_project.service;
 
 import lk.ijse.aad_project.dto.UserDTO;
  
-public interface UserService {
+public interface UserService { 
     void saveUser(UserDTO userDTO);
     void updateUser(UserDTO userDTO);
     void removeUser(long userId);
