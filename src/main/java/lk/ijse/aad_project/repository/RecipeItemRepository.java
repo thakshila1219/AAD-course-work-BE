4f0b56ac2e47223ce452515f488be04ee879e0cd
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:14:55 +0530

    feat(repository): create RecipeItemRepository for recipe mapping queries

--- src/main/java/lk/ijse/aad_project/repository/RecipeItemRepository.java
+++ src/main/java/lk/ijse/aad_project/repository/RecipeItemRepository.java
@@ -3,7 +3,7 @@ package lk.ijse.aad_project.repository;
 import lk.ijse.aad_project.entity.RecipeItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 @Repository
 public interface RecipeItemRepository extends JpaRepository<RecipeItem, Long> {
 }
