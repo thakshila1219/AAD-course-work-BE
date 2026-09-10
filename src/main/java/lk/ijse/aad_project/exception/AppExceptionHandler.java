2a02d89f02a91f1d4c7b8112e2909302f0649da8
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 21:18:08 +0530

    feat(exception): implement global exception handler with ControllerAdvice

--- src/main/java/lk/ijse/aad_project/exception/AppExceptionHandler.java
+++ src/main/java/lk/ijse/aad_project/exception/AppExceptionHandler.java
@@ -1,7 +1,7 @@
 package lk.ijse.aad_project.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity; 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
