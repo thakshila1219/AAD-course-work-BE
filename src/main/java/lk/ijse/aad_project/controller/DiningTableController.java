364a76b064b975ecec25a61b4289592711f8d711
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 20:11:29 +0530

    add endpoints for table management and status updates

--- src/main/java/lk/ijse/aad_project/controller/DiningTableController.java
+++ src/main/java/lk/ijse/aad_project/controller/DiningTableController.java
@@ -1,7 +1,7 @@
 package lk.ijse.aad_project.controller;
 
 import lk.ijse.aad_project.dto.DiningTableDTO;
-import lk.ijse.aad_project.service.DiningTableService;
+import lk.ijse.aad_project.service.DiningTableService; 
 import lk.ijse.aad_project.contant.CommonResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
