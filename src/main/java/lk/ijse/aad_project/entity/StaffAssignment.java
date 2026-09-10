fa3758d6ff5dab0415384c838a82404329f025d5
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 20:44:06 +0530

    feat(entity): create StaffAssignment entity for duty scheduling

--- src/main/java/lk/ijse/aad_project/entity/StaffAssignment.java
+++ src/main/java/lk/ijse/aad_project/entity/StaffAssignment.java
@@ -3,7 +3,7 @@ package lk.ijse.aad_project.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor; 
 import java.time.LocalDate;
 
 @Data
