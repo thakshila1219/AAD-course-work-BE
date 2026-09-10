8c7a2f951238b50cd9847ba1101495084826a72d
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:17:03 +0530

    feat(repository): update UserRepository with custom email lookup query

--- src/main/java/lk/ijse/aad_project/repository/UserRepository.java
+++ src/main/java/lk/ijse/aad_project/repository/UserRepository.java
@@ -3,7 +3,7 @@ package lk.ijse.aad_project.repository;
 import lk.ijse.aad_project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 import java.util.Optional;
 
 @Repository
