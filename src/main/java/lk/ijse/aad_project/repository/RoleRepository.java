ddc2adcef20fbed4380706779cbdd75734f94a57
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:15:48 +0530

    feat(repository): add RoleRepository for Spring Security authorization

--- src/main/java/lk/ijse/aad_project/repository/RoleRepository.java
+++ src/main/java/lk/ijse/aad_project/repository/RoleRepository.java
@@ -3,7 +3,7 @@ package lk.ijse.aad_project.repository;
 import lk.ijse.aad_project.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
