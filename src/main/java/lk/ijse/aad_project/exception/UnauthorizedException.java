2f5c82268a365eee8c6f0a08bdf50c77e3ce1724
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 21:24:02 +0530

    feat(exception): add UnauthorizedException for authentication failures

--- src/main/java/lk/ijse/aad_project/exception/UnauthorizedException.java
+++ src/main/java/lk/ijse/aad_project/exception/UnauthorizedException.java
@@ -3,5 +3,5 @@ package lk.ijse.aad_project.exception;
 public class UnauthorizedException extends RuntimeException {
     public UnauthorizedException(String message) {
         super(message);
-    }
+    } 
 }
