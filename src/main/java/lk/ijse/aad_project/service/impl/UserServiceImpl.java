fa22dd35979894379e1d52414653aa28b2ef3f25
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 23:02:31 +0530

    feat(service): update UserServiceImpl with password encoding and user management

--- src/main/java/lk/ijse/aad_project/service/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/aad_project/service/impl/UserServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Service
 @Slf4j
 public class UserServiceImpl implements UserService {
-
+ 
     private final UserRepository userRepository;
 
     public UserServiceImpl(UserRepository userRepository) {
