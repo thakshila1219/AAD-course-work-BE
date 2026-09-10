3419f372c08a393199f12b0ff9b367b5c87dec28
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 21:23:41 +0530

    feat(exception): create ResourceNotFoundException for missing entity lookups

--- src/main/java/lk/ijse/aad_project/exception/ResourceNotFoundException.java
+++ src/main/java/lk/ijse/aad_project/exception/ResourceNotFoundException.java
@@ -3,5 +3,5 @@ package lk.ijse.aad_project.exception;
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
-    }
+    } 
 }
