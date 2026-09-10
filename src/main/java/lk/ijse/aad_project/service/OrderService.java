349d40b35c8eb167b7496810cdd4ed21aef18885
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:38:30 +0530

    feat(service): add OrderService interface for order processing logic

--- src/main/java/lk/ijse/aad_project/service/OrderService.java
+++ src/main/java/lk/ijse/aad_project/service/OrderService.java
@@ -4,7 +4,7 @@ import lk.ijse.aad_project.dto.OrderDTO;
 
 public interface OrderService {
     void saveOrder(OrderDTO orderDTO);
-    void updateOrder(OrderDTO orderDTO);
+    void updateOrder(OrderDTO orderDTO); 
     void removeOrder(long orderId);
 
     void updateOrderStatus(long orderId);
