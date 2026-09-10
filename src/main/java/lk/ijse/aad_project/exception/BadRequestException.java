2ae03f86267b8242c5fddb75f88d2710f40d8afb
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 21:20:20 +0530

    feat(exception): create BadRequestException for invalid API payload handling

--- src/main/java/lk/ijse/aad_project/exception/BadRequestException.java
+++ src/main/java/lk/ijse/aad_project/exception/BadRequestException.java
@@ -3,5 +3,5 @@ package lk.ijse.aad_project.exception;
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
         super(message);
-    }
+    } 
 }
