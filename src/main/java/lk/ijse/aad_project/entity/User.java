ed6823303f992275aa2c1500583027302c29c414
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 20:44:53 +0530

    feat(entity): update User entity with authentication and role mapping

--- src/main/java/lk/ijse/aad_project/entity/User.java
+++ src/main/java/lk/ijse/aad_project/entity/User.java
@@ -3,7 +3,7 @@ package lk.ijse.aad_project.entity;
 import com.fasterxml.jackson.annotation.JsonProperty; 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Data; 
 import lombok.NoArgsConstructor;
 import java.util.List;
 
