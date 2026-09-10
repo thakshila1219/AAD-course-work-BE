6444d8f1ac45d5a49859a7b4be1f7710b764b866
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:45:15 +0530

    feat(service): add RoleService interface for role management

--- src/main/java/lk/ijse/aad_project/service/RoleService.java
+++ src/main/java/lk/ijse/aad_project/service/RoleService.java
@@ -3,7 +3,7 @@ package lk.ijse.aad_project.service;
 import lk.ijse.aad_project.dto.RoleDTO;
 
 public interface RoleService {
-    void saveRole(RoleDTO roleDTO);
+    void saveRole(RoleDTO roleDTO); 
     void updateRole(RoleDTO roleDTO);
     void removeRole(long roleId);
 }
