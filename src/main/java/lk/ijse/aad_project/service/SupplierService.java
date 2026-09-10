6fbc72655784768a122427b73bb2a0eca244bbb5
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:48:18 +0530

    feat(service): add SupplierService interface for vendor management

--- src/main/java/lk/ijse/aad_project/service/SupplierService.java
+++ src/main/java/lk/ijse/aad_project/service/SupplierService.java
@@ -2,7 +2,7 @@ package lk.ijse.aad_project.service;
 
 import lk.ijse.aad_project.dto.SupplierDTO;
 
-public interface SupplierService {
+public interface SupplierService { 
     void saveSupplier(SupplierDTO supplierDTO);
     void updateSupplier(SupplierDTO supplierDTO);
     void removeSupplier(long supplierId);
