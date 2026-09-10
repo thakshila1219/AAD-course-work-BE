24a13a1271cdde91a16fb2179550bdd324af65a3
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:11:16 +0530

    feat(repository): create OrderDetailRepository for order items breakdown

--- src/main/java/lk/ijse/aad_project/repository/OrderDetailRepository.java
+++ src/main/java/lk/ijse/aad_project/repository/OrderDetailRepository.java
@@ -3,7 +3,7 @@ package lk.ijse.aad_project.repository;
 import lk.ijse.aad_project.entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
 }
