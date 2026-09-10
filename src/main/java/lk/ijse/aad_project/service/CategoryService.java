b83d0a33a6eaad81da091a199b759da6871064af
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:35:32 +0530

    feat(service): add CategoryService interface for category operations

--- src/main/java/lk/ijse/aad_project/service/CategoryService.java
+++ src/main/java/lk/ijse/aad_project/service/CategoryService.java
@@ -1,7 +1,7 @@
 package lk.ijse.aad_project.service;
 
 import lk.ijse.aad_project.dto.CategoryDTO;
-
+ 
 public interface CategoryService {
     void saveCategory(CategoryDTO categoryDTO);
     void updateCategory(CategoryDTO categoryDTO);
