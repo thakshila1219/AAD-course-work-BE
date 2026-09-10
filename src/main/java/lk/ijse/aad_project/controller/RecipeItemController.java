7893907f5a53e53bba0c496a8859645588504a93
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 20:18:56 +0530

    implement recipe ingredient mapping endpoints

--- src/main/java/lk/ijse/aad_project/controller/RecipeItemController.java
+++ src/main/java/lk/ijse/aad_project/controller/RecipeItemController.java
@@ -1,7 +1,7 @@
 package lk.ijse.aad_project.controller;
 
 import lk.ijse.aad_project.dto.RecipeItemDTO;
-import lk.ijse.aad_project.service.RecipeItemService;
+import lk.ijse.aad_project.service.RecipeItemService; 
 import lk.ijse.aad_project.contant.CommonResponse; 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
