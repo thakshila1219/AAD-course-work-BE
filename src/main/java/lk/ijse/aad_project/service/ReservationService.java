9a0a8f7f06ed716f6436a63dbb04bf837e248587
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:40:44 +0530

    feat(service): add RoleService interface for role management

--- src/main/java/lk/ijse/aad_project/service/ReservationService.java
+++ src/main/java/lk/ijse/aad_project/service/ReservationService.java
@@ -5,7 +5,7 @@ import lk.ijse.aad_project.dto.ReservationDTO;
 public interface ReservationService {
     void saveReservation(ReservationDTO reservationDTO); 
     void updateReservation(ReservationDTO reservationDTO);
-    void removeReservation(long reservationId);
+    void removeReservation(long reservationId); 
 
     void cancelReservation(long reservationId);
 }
