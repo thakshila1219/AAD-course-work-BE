3702344f95d1a2b20ec5b1997ec758337e3c34a0
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:46:11 +0530

    feat(service): create StaffAssignmentService interface for duty allocation

--- src/main/java/lk/ijse/aad_project/service/StaffAssignmentService.java
+++ src/main/java/lk/ijse/aad_project/service/StaffAssignmentService.java
@@ -3,7 +3,7 @@ package lk.ijse.aad_project.service;
 import lk.ijse.aad_project.dto.StaffAssignmentDTO;
 
 public interface StaffAssignmentService {
-    void saveStaffAssignment(StaffAssignmentDTO staffAssignmentDTO);
+    void saveStaffAssignment(StaffAssignmentDTO staffAssignmentDTO); 
     void updateStaffAssignment(StaffAssignmentDTO staffAssignmentDTO);
     void removeStaffAssignment(long assignmentId);
 }
