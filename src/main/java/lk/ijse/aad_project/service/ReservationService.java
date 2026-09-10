9df47afb93eaca183b2372b3d3c2779039616328
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:44:52 +0530

    feat(service): create ReservationService interface for booking workflows

--- src/main/java/lk/ijse/aad_project/service/ReservationService.java
+++ src/main/java/lk/ijse/aad_project/service/ReservationService.java
@@ -2,7 +2,7 @@ package lk.ijse.aad_project.service;
 
 import lk.ijse.aad_project.dto.ReservationDTO;
 
-public interface ReservationService {
+public interface ReservationService { 
     void saveReservation(ReservationDTO reservationDTO); 
     void updateReservation(ReservationDTO reservationDTO);
     void removeReservation(long reservationId); 
