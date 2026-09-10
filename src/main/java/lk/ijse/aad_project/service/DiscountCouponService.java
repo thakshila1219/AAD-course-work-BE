a12f49e4c4e9212c60ecf5a551ca6abb0376d8b8
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:36:37 +0530

    feat(service): add DiscountCouponService interface for coupon business logic

--- src/main/java/lk/ijse/aad_project/service/DiscountCouponService.java
+++ src/main/java/lk/ijse/aad_project/service/DiscountCouponService.java
@@ -5,5 +5,5 @@ import lk.ijse.aad_project.dto.DiscountCouponDTO;
 public interface DiscountCouponService {
     void saveDiscountCoupon(DiscountCouponDTO discountCouponDTO);
     void updateDiscountCoupon(DiscountCouponDTO discountCouponDTO);
-    void removeDiscountCoupon(long couponId);
+    void removeDiscountCoupon(long couponId); 
 }
