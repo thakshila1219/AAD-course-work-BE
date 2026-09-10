05f44878af6309d67dec646d574333f8a33682b0
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:37:32 +0530

    feat(service): add MenuItemService interface for menu item operations

--- src/main/java/lk/ijse/aad_project/service/MenuItemService.java
+++ src/main/java/lk/ijse/aad_project/service/MenuItemService.java
@@ -4,6 +4,6 @@ import lk.ijse.aad_project.dto.MenuItemDTO;
 
 public interface MenuItemService {
     void saveMenuItem(MenuItemDTO menuItemDTO);
-    void updateMenuItem(MenuItemDTO menuItemDTO);
+    void updateMenuItem(MenuItemDTO menuItemDTO); 
     void removeMenuItem(long itemId);
 }
