c560633fd269f2bd88b0b3eb75bb5ad10bb6580b
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:14:34 +0530

    feat(repository): add PaymentRepository for payment verification

--- src/main/java/lk/ijse/aad_project/repository/PaymentRepository.java
+++ src/main/java/lk/ijse/aad_project/repository/PaymentRepository.java
@@ -3,7 +3,7 @@ package lk.ijse.aad_project.repository;
 import lk.ijse.aad_project.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
