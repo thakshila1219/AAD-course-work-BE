2fd8b4981400da0919f9888c6510b33fe702f63c
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 19:47:44 +0530

    feat(response): add custom application status and error codes

--- src/main/java/lk/ijse/aad_project/contant/ResponseStatusCode.java
+++ src/main/java/lk/ijse/aad_project/contant/ResponseStatusCode.java
@@ -1,6 +1,6 @@
 package lk.ijse.aad_project.contant;
 
-public class ResponseStatusCode {
+public class ResponseStatusCode { 
 
     public static int OPERATION_SUCCESS = 0;
     public static int OPERATION_FAILED = 1;
