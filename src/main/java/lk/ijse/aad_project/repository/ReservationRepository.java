a9551cad51473fd0e5e4fea4fbfedb863a06a2c8
Thakshila Mdushani <thakshilamadushani42@gmail.com>
2026-09-10 22:15:23 +0530

    feat(repository): implement ReservationRepository for table booking queries

--- src/main/java/lk/ijse/aad_project/repository/ReservationRepository.java
+++ src/main/java/lk/ijse/aad_project/repository/ReservationRepository.java
@@ -3,7 +3,7 @@ package lk.ijse.aad_project.repository;
 import lk.ijse.aad_project.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
